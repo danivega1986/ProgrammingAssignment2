788bd1961cd5c7775512913f8488ecf513b882bb
Daniel V <danivega1986@gmail.com>
2020-07-17 21:40:45 -0500

    first advance of the CacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL                             ##Initialized inv value as a NUll,
+                                                ##It's going to keep the inverse matrix
+        
+        set <- function(y) {                    ##Define the set function to assign a new
+                ##value in the parent environment
+                x <<- y                         ##Define the matrix in the parent environment
+                inv <<- NULL                    ##IF there is a new matrix then set inv as a NUll
+         
+        }       
+        
+        get <- function() x                     ##This returns the value of the matrix arguments
+                
+        setinverse <- function(inverse) inv <<- inverse
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)        
+        
 }
 
 
